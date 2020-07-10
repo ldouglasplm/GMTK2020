b6cffab36e34cbd61685e624c1ff6e1a83fdf421
Felix Curran <furraxcelin@hotmail.com>
2020-07-10 08:50:31 +0100

    First test adding new files after removing cache

---
+++ GAME/Assets/Files/FelixTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FelixTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GITIGNORE NOW WORKING?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
