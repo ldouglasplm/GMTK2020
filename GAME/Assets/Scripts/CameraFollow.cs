f01c7476c870baabeed23d5f5ed911e06217bff1
Felix Curran <furraxcelin@hotmail.com>
2020-07-12 18:33:06 +0100

    Changed camera to look at boulder

--- GAME/Assets/Scripts/CameraFollow.cs
+++ GAME/Assets/Scripts/CameraFollow.cs
@@ -1,20 +1,37 @@
 using UnityEngine;
 using System.Collections;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Collections.Specialized;
 
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;
+    public Transform target;
+
     // Use this for initialization
+
     void Start()
     {
-        offset = transform.position;
+        
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
         player = GameObject.FindWithTag("Boulder");
-        transform.position = player.transform.position + offset;
+        target = player.transform;
+
+        if (StartingPosition.gameStart == false)
+        {
+            //transform.position = new Vector3 (0f, 53f, 0f);
+            transform.LookAt(target);
+        }
+
+        if (StartingPosition.gameStart == true)
+        {
+            //transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, target.transform.position.z - target.transform.position.z - target.transform.position.z);
+            transform.LookAt(target);
+        }
+
     }
 }
