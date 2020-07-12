29d2fae869080a85ac9758556b7a7ad62553656c
Felix Curran <furraxcelin@hotmail.com>
2020-07-12 19:42:00 +0100

    Final Build

--- GAME/Assets/Scripts/BoulderCollision.cs
+++ GAME/Assets/Scripts/BoulderCollision.cs
@@ -9,29 +9,29 @@ public class BoulderCollision : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag.Equals("Boulder") == true)
+        if (collision.gameObject.tag.Equals("Boulder") == true || collision.gameObject.tag.Equals("FinishedBoulder") == true)
         {
             Rigidbody myRigidbody = this.GetComponent<Rigidbody>();
             myRigidbody.isKinematic = false;
 
             if (gameObject.tag.Equals("1 Point") == true && gameObject.tag != ("collided"))
             {
-                ScoreManager.score = ScoreManager.score + 1;
+                ScoreManager.score = ScoreManager.score + 1 * 13;
             }
             
             if (gameObject.tag.Equals("2 Point") == true && gameObject.tag != ("collided"))
             {
-                ScoreManager.score = ScoreManager.score + 2;
+                ScoreManager.score = ScoreManager.score + 2 * 13;
             }
 
             if (gameObject.tag.Equals("3 Point") == true && gameObject.tag != ("collided"))
             {
-                ScoreManager.score = ScoreManager.score + 3;
+                ScoreManager.score = ScoreManager.score + 3 * 13;
             }
 
             if (gameObject.tag.Equals("50 Point") == true && gameObject.tag != ("collided"))
             {
-                ScoreManager.score = ScoreManager.score + 50;
+                ScoreManager.score = ScoreManager.score + 50 * 13;
             }
 
             myRigidbody.tag = "collided";
